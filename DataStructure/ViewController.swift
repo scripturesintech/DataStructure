7a132e8ef71ae82c4e785703b021bc4a251c6227
Roopesh Tripathi <123146786+scripturesintech@users.noreply.github.com>
2024-08-22 13:45:19 +0530

    Update ViewController.swift

--- DataStructure/ViewController.swift
+++ DataStructure/ViewController.swift
@@ -525,6 +525,37 @@ func maxSubArray(_ nums: [Int]) -> Int {
     return globalMax
 }
 
+    func mergeSortedArrays(_ array1: [Int], _ array2: [Int]) -> [Int] {
+    var mergedArray: [Int] = []
+    var i = 0  // Pointer for array1
+    var j = 0  // Pointer for array2
+
+    // Step 1: Merge the arrays while both pointers are within bounds
+    while i < array1.count && j < array2.count {
+        if array1[i] < array2[j] {
+            mergedArray.append(array1[i])
+            i += 1
+        } else {
+            mergedArray.append(array2[j])
+            j += 1
+        }
+    }
+
+    // Step 2: Add the remaining elements from array1, if any
+    while i < array1.count {
+        mergedArray.append(array1[i])
+        i += 1
+    }
+
+    // Step 3: Add the remaining elements from array2, if any
+    while j < array2.count {
+        mergedArray.append(array2[j])
+        j += 1
+    }
+
+    return mergedArray
+}
+
 
 }
 
